64a52dba668d40fab5829ad1957e3d1322d01970
changkaiyan <894076259@qq.com>
2020-03-03 20:10:45 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-class MyModel(tf.keras.Model):
-
-  def __init__(self):
-    super(MyModel, self).__init__()
-    self.dense1 = tf.keras.layers.[Dense Like Layer]
-
-  def call(self, inputs):
-    x = self.dense1(inputs)
-    return x
